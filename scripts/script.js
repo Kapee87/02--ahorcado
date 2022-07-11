262218f6852ae41fd6ac2fedd451450646553db7
Kapee87 <kapserker@gmail.com>
2022-07-11 14:43:14 -0300

    Muestra copa al ganar nivel final

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,100 @@
 const diccionario = [
-"marcar", 
-"generoso", 
-"bebida", 
-"frontal",
-"loco", 
-"proceder", 
-"fundador" ,
-"lucha",
-"hueso" ,
-"experimental" ,
-"oveja",
-"clasificar",
-"magia" ,
-"inconsciente" ,
-"marchar",
-"enfoque",
-"circulo" ,
-"demostracion" ,
-"idolo" ,
-"fallecimiento",
-"reportes" ,
-"golf" ,
-"identificado" ,
-"habilidad",
-"estricto" ,
-"prisa" ,
-"pulmones" ,
-"rueda",
-"paralelogramo",
-"trueno",
-"cascanueces",
-"auto",
-"desarrollo",
-"romper",
-"joroba",
-"escoba",
-"hielo",
-"miel",
-"pan",
-"kiwi",
-"oso",
-"locro",
-"amor",
-"jugo",
-"lana",
-"pata",
-"pico",
-"mano",
-"heno",
-"lija",
-"te",
-"sol",
-"luna",
-"as",
-"yo"
+    "marcar",
+    "generoso",
+    "bebida",
+    "frontal",
+    "loco",
+    "proceder",
+    "fundador",
+    "lucha",
+    "hueso",
+    "experimental",
+    "oveja",
+    "clasificar",
+    "magia",
+    "inconsciente",
+    "marchar",
+    "enfoque",
+    "circulo",
+    "demostracion",
+    "idolo",
+    "fallecimiento",
+    "reportes",
+    "golf",
+    "identificado",
+    "habilidad",
+    "estricto",
+    "prisa",
+    "pulmones",
+    "rueda",
+    "paralelogramo",
+    "trueno",
+    "cascanueces",
+    "auto",
+    "desarrollo",
+    "romper",
+    "joroba",
+    "escoba",
+    "hielo",
+    "miel",
+    "pan",
+    "kiwi",
+    "oso",
+    "locro",
+    "amor",
+    "jugo",
+    "lana",
+    "pata",
+    "pico",
+    "mano",
+    "heno",
+    "lija",
+    "te",
+    "sol",
+    "luna",
+    "as",
+    "yo",
+    "danzarin",
+    "danzaron",
+    "danzaste",
+    "dañarias",
+    "dañarlas",
+    "dañarlos",
+    "dañarnos",
+    "dariamos",
+    "darmela",
+    "darnosla",
+    "darsenas",
+    "ecuacion",
+    "ecualice",
+    "elaboren",
+    "elabores",
+    "elástica",
+    "encalvar", 
+    "encalzar",
+    "palentino", 
+    "paleoceno", 
+    "paleogeno", 
+    "paleolito", 
+    "paleologa", 
+    "paleologo", 
+    "palestina", 
+    "palestino", 
+    "palestras", 
+    "paletadas", 
+    "paletazos", 
+    "paletera",
+    "paleteros", 
+    "paletilla", 
+    "paletones", 
 ];
 
-let palabra="";
+let palabra = "";
 let dificultad = 0;
 // const palabraSecreta = document.querySelector("#palabraSecreta"); Uso para control interno
 const $facil = document.querySelector("#Nfacil");
-const $medio=document.querySelector("#Nmedio");
-const $dificil=document.querySelector("#Ndificil"); 
+const $medio = document.querySelector("#Nmedio");
+const $dificil = document.querySelector("#Ndificil");
 const nivel = document.querySelectorAll(".dificultad");
 const $indicador = document.querySelector("#indicador");
 const $tiempo = document.querySelector("#tiempo");
@@ -69,32 +102,33 @@ const $reglas = document.querySelector('#reglas');
 const $aiuda = document.querySelector('#aiuda');
 const $estadoAnterior = document.querySelector("#estadoAnterior")
 let pantalla = document.querySelector("canvas");
+let $copa = document.querySelector('#copa');
 
 let pincel = pantalla.getContext("2d");
-let startGamebtn= document.querySelector("#startGame");
+let startGamebtn = document.querySelector("#startGame");
 let indice = 0;
 let conteo;
-let restante=0;
-let centerX= pantalla.width/8;
-let intentos=0;
-let contadorExito=1;
-let palabraTemp= [];
+let restante = 0;
+let centerX = pantalla.width / 8;
+let intentos = 0;
+let contadorExito = 1;
+let palabraTemp = [];
 let chr;
-let repetida=[];
+let repetida = [];
 
 
 // funciones
-function crearTablero(){
-    
-// pincel.fillStyle = "#080908";
-pincel.fillStyle = "white";
-pincel.fillRect(0,0,1000, 1000);
+function crearTablero() {
+
+    // pincel.fillStyle = "#080908";
+    pincel.fillStyle = "white";
+    pincel.fillRect(0, 0, 1000, 1000);
 
 }
 
 
 
-function alternarVisibilidad(){
+function alternarVisibilidad() {
     startGamebtn.classList.toggle("hidden");
     $facil.classList.toggle('hidden');
     $medio.classList.toggle('hidden');
@@ -104,118 +138,130 @@ function alternarVisibilidad(){
     $aiuda.classList.add('hidden');
     pantalla.classList.toggle('stand-by');
     pantalla.classList.toggle('jugar');
+
+}
+function alternarVisibilidadOff() {
+    startGamebtn.classList.remove("hidden");
+    $facil.classList.remove('hidden');
+    $medio.classList.remove('hidden');
+    $dificil.classList.remove('hidden');
+    $indicador.classList.add('hidden');
+    $reglas.classList.remove('hidden');
+    $aiuda.classList.add('hidden');
+    pantalla.classList.add('stand-by');
+    pantalla.classList.remove('jugar');
     
-}
-
-function palabraRandom(){
-     let sw=0;
-     let ref = 100;
-     while (sw===0 && ref > 0){
-                        indice = indiceAleatorio();
-                        if (dificultad === 0 && diccionario[indice].length < 5){
-                            imprimirValor();
-
-                        }else if (dificultad === 1 && diccionario[indice].length > 4 && diccionario[indice].length < 8){
-                            imprimirValor();
-                        }if (dificultad === 2 && diccionario[indice].length > 7){
-                            imprimirValor();
-                        }
-                        ref--;
-                    }
-                    
-        return palabra;
- 
-}
-
-function guiones(cantidad){
-
-pincel.fillStyle= "black";
-pincel.font = "2em Ruslan Display cursive";
-
-for ( let i=0; i < cantidad; i++){
-    pincel.fillText("-", centerX + i*20, 145, 800)
-}
 
 }
 
-function imprimirValor(){
+function palabraRandom() {
+    let sw = 0;
+    let ref = 100;
+    while (sw === 0 && ref > 0) {
+        indice = indiceAleatorio();
+        if (dificultad === 0 && diccionario[indice].length < 5) {
+            imprimirValor();
+
+        } else if (dificultad === 1 && diccionario[indice].length > 4 && diccionario[indice].length < 8) {
+            imprimirValor();
+        } if (dificultad === 2 && diccionario[indice].length > 7) {
+            imprimirValor();
+        }
+        ref--;
+    }
+
+    return palabra;
+
+}
+
+function guiones(cantidad) {
+
+    pincel.fillStyle = "black";
+    pincel.font = "2em Ruslan Display cursive";
+
+    for (let i = 0; i < cantidad; i++) {
+        pincel.fillText("-", centerX + i * 20, 145, 800)
+    }
+
+}
+
+function imprimirValor() {
     sw = 1;
     ref = 0;
-    palabra= diccionario[indice].toUpperCase();
-    
+    palabra = diccionario[indice].toUpperCase();
+
     // palabraSecreta.innerHTML = palabra +' ' + palabra.length; Uso para control interno
-    
+
 }
-function indiceAleatorio(){
-    let largo= diccionario.length;
-    
+function indiceAleatorio() {
+    let largo = diccionario.length;
+
     indice = Math.round(Math.random() * (largo - 1));
     return indice;
 }
 
-function elegirDificultad(nivelInterno){
+function elegirDificultad(nivelInterno) {
     dificultad = nivelInterno;
     restante = 80 - (dificultad * 30)
 
     // alert(dificultad);
     resetearEstiloNivel();
-    nivel[nivelInterno].className =' dificultad dificultadSelect';
+    nivel[nivelInterno].className = ' dificultad dificultadSelect';
 }
-function resetearEstiloNivel(){
-    for(let i=0; i < nivel.length; i++){
-        nivel[i].className='dificultad';
+function resetearEstiloNivel() {
+    for (let i = 0; i < nivel.length; i++) {
+        nivel[i].className = 'dificultad';
     }
     // alert('litos')
 }
-function iniciarTiempo(){
-    if ($tiempo.textContent > 0){
+function iniciarTiempo() {
+    if ($tiempo.textContent > 0) {
         $tiempo.textContent--;
-    }else{
+    } else {
         juegoTerminado();
     }
 
 }
 
-function juegoTerminado(){ 
+function juegoTerminado() {
+    $estadoAnterior.classList.remove('hidden');
     clearInterval(conteo);
     resetearGeneral();
-    alternarVisibilidad();
-    $estadoAnterior.classList.remove('hidden');
-    resetearEstiloNivel();
+    alternarVisibilidadOff();
     elegirDificultad(0);
 }
 
-function resetearGeneral(){
-    palabra= "";
-    dificultad=0;
-    indice=0;
-    conteo=0;
-    $tiempo.textContent=0;
+function resetearGeneral() {
+    palabra = "";
+    dificultad = 0;
+    indice = 0;
+    conteo = 0;
+    $tiempo.textContent = 0;
     // palabraSecreta.innerHTML = palabra; Uso para control interno
-    intentos=0;
-    contadorExito=1;
-    repetida=[];
-    palabraTemp=[];
-    
+    intentos = 0;
+    contadorExito = 1;
+    repetida = [];
+    palabraTemp = [];
+
 
 }
 
-function dibujar(contador){
-    switch (contador){
+function dibujar(contador) {
+    switch (contador) {
         case 0:
             pincel.fillStyle = "black";
             pincel.fillRect(20, 110, 150, 10);
             pincel.fillRect(50, 10, 10, 100);
-            pincel.fillRect(50, 8, 100, 10);    
+            pincel.fillRect(50, 8, 100, 10);
             pincel.fillRect(136.5, 12, 11, 15);
             pincel.fillRect(134, 22, 15, 2);
-            pincel.fillRect(134, 19, 15, 2);        
+            pincel.fillRect(134, 19, 15, 2);
             break;
         case 1:
             // cabeza
             pincel.beginPath();
-            pincel.arc(142,35,10,0,Math.PI*2,true);
-            pincel.moveTo(110,75);
+            pincel.arc(142, 35, 10, 0, Math.PI * 2, true);
+            pincel.moveTo(110, 75);
             pincel.stroke();
             break;
         case 2:
@@ -250,129 +296,145 @@ function dibujar(contador){
             pincel.lineTo(120, 95);
             pincel.stroke();
             break;
-            case 7:
-                pincel.fillRect(128, 43, 40, 2)
-                break;
+        case 7:
+            pincel.fillRect(128, 43, 40, 2)
+            break;
     }
 }
 
-function verificarLetra(e){
+function verificarLetra(e) {
     chr = String.fromCharCode(e.which).toUpperCase();
-    
-    let repetidAcertada=0;
 
-        if ( intentos === 0){
-           
-       
-         
-        for (let i=0; i < palabra.length; i++){
-            palabraTemp[i]= palabra[i].toUpperCase();    
+    let repetidAcertada = 0;
+
+    if (intentos === 0) {
+
+
+
+        for (let i = 0; i < palabra.length; i++) {
+            palabraTemp[i] = palabra[i].toUpperCase();
         }
-        }
-        
+    }
+
     if (esValido(e.which) && !repetida.includes(chr)) {
         let pifiar = true;
-        for (let i=0; i < palabra.length; i++){
+        for (let i = 0; i < palabra.length; i++) {
             // alert(chr + "" + palabraTemp[i])
-            if (chr === palabraTemp[i]){
-                 letraCorrecta(palabraTemp[i], i);
+            if (chr === palabraTemp[i]) {
+                letraCorrecta(palabraTemp[i], i);
                 palabraTemp[i] = "-"
-               
-                
+
+
                 // palabraSecreta.innerHTML= palabraTemp[i]; Uso para control interno
-                pifiar=false; 
-                repetidAcertada=repetidAcertada+1;
-                          
+                pifiar = false;
+                repetidAcertada = repetidAcertada + 1;
+
             }
         }
-        if (!repetida.includes(chr)){
+        if (!repetida.includes(chr)) {
             console.log(repetida.includes(chr))
-            if (pifiar){
+            if (pifiar) {
                 dibujar(intentos);
                 LetraIncorrecta(chr, repetida.length)
-                    intentos++;
-                    repetida.push(chr);
-                    return verificarPerdedor(pifiar);
-            }else{
+                intentos++;
+                repetida.push(chr);
+                return !verificarGanador(repetidAcertada, pifiar) ? verificarPerdedor(pifiar) : "";
+            } else {
                 return verificarGanador(repetidAcertada, pifiar);
             }
-    
+
         }
-     
-   }
-   
+
     }
 
-    function letraCorrecta(letra, indice){
-        pincel.fillStyle= "black";
-        pincel.font = "1em Ruslan Display cursive";
-        for (let i=0; i< 8;i++){
-            pincel.fillText(letra, centerX + indice*20, 135, 20)
-        }
-       
-    }
-    function LetraIncorrecta(letra, indice){
-        pincel.fillStyle= 'red'
-        pincel.font = ".7em Russlan Display cursive";
-        
-        pincel.fillText(letra, centerX+ indice*20 , 150, 15);
-        console.log(letra + " es la letra")
+}
+
+function letraCorrecta(letra, indice) {
+    pincel.fillStyle = "black";
+    pincel.font = "1em Ruslan Display cursive";
+    for (let i = 0; i < 8; i++) {
+        pincel.fillText(letra, centerX + indice * 20, 135, 20)
     }
 
-    function verificarGanador(repetidAcertada, pifiar){
-     if (contadorExito === palabra.length || contadorExito > palabra.length){
-         console.log("ha ganau desgraciau!");
-         clearTablero();
-         dibujarMensajeFinal("Ganaste guachi!");
-         clearInterval(conteo);
-         $estadoAnterior.innerHTML = ` ${dificultad < 2 ? 'La ultima vez Ganaste, Subi el nivel': 'Felicitaciones! Ganaste el máximo nivel!'}`;
-         pincel.fillStyle="white";
-         setTimeout(juegoTerminado, 1000);
-         mostrarWinLooser();
-     }else if(!pifiar) {
-        contadorExito+= repetidAcertada;
+}
+function LetraIncorrecta(letra, indice) {
+    pincel.fillStyle = 'red'
+    pincel.font = ".7em Russlan Display cursive";
+
+    pincel.fillText(letra, centerX + indice * 20, 150, 15);
+    console.log(letra + " es la letra")
+}
+
+function verificarGanador(repetidAcertada, pifiar) {
+    if (contadorExito === palabra.length || contadorExito > palabra.length) {
+        console.log("ha ganau desgraciau!");
+        clearTablero();
+        dibujarMensajeFinal("Ganaste guachi!");
+        clearInterval(conteo);
+        $estadoAnterior.innerHTML = verificarDifiWin();
+
+        pincel.fillStyle = "white";
+        setTimeout(juegoTerminado, 1000);
+        mostrarWinLooser();
+    } else if (!pifiar) {
+        contadorExito += repetidAcertada;
         repetida.push(chr);
-     }
-        console.log(contadorExito, palabra.length + " control de cExito y length")
     }
+    console.log(contadorExito, palabra.length + " control de cExito y length")
+}
 
-    function verificarPerdedor(pifiar){
-        
-     if (intentos === 8){
+function verificarDifiWin() {
+
+    if (dificultad < 2) {
+        return 'La ultima vez Ganaste, Subi el nivel'
+    } else {
+        setTimeout(() => { $copa.classList.toggle('hidden') }, 1000)
+        return 'Felicitaciones! Ganaste el máximo nivel!'
+
+    };
+
+
+
+}
+
+function verificarPerdedor(pifiar) {
+
+    if (intentos === 8) {
         clearTablero();
         console.log("ha perdiu desgraciau!");
         dibujarMensajeFinal("Perdiste. LOOOOOOSER!")
         clearInterval(conteo);
-        setTimeout(juegoTerminado, 1000);
         $estadoAnterior.innerHTML = 'La ultima vez Perdiste';
+        setTimeout(juegoTerminado, 1000);
         mostrarWinLooser();
-    }else if(!pifiar) {
-       contadorExito++;
+    } else if (!pifiar) {
+        contadorExito++;
     }
-       console.log(contadorExito, palabra.length + " control de cExito y length")
-    }
-    function dibujarMensajeFinal(msj){
-        pincel.fillStyle= 'black'
-        pincel.fillText(msj, 40, 50, 100);
+    console.log(contadorExito, palabra.length + " control de cExito y length")
+}
+function dibujarMensajeFinal(msj) {
+    pincel.fillStyle = 'black'
+    pincel.fillText(msj, 40, 50, 100);
 
-        console.log(msj+' este es el mensaje')
-    }
-  
-    function mostrarWinLooser(){
-        
-        const temporizadorWinLooser = setInterval($estadoAnterior.classList.add('colormation'), 500);
-        setTimeout(()=>{
-            clearInterval(temporizadorWinLooser)
-            $estadoAnterior.classList.toggle('hidden');
-            $estadoAnterior.classList.toggle('colormation')
-        }, 5000 );
-        
-    }
-  
-    function clearTablero(){
-        pincel.fillStyle = "white";
-        pincel.fillRect(0,0,1000,1000);
-    }
+    console.log(msj + ' este es el mensaje')
+}
+
+function mostrarWinLooser() {
+
+    const temporizadorWinLooser = setInterval($estadoAnterior.classList.add('colormation'), 500);
+    setTimeout(() => {
+        clearInterval(temporizadorWinLooser)
+        $estadoAnterior.classList.toggle('hidden');
+        $estadoAnterior.classList.toggle('colormation');
+        $copa.classList.toggle('hidden');
+    }, 5000);
+
+}
+
+function clearTablero() {
+    pincel.fillStyle = "white";
+    pincel.fillRect(0, 0, 1000, 1000);
+}
 
 
 
@@ -384,7 +446,7 @@ function esValido(c) {
 // eventos
 
 
-startGamebtn.addEventListener('click', () =>{
+startGamebtn.addEventListener('click', () => {
     crearTablero();
     palabraRandom();
     alternarVisibilidad();
@@ -393,13 +455,13 @@ startGamebtn.addEventListener('click', () =>{
     guiones(palabra.length);
     // dibujar();
     $estadoAnterior.classList.add('hidden');
-    window.addEventListener('keypress', (e)=>{verificarLetra(e)} );
+    window.addEventListener('keypress', (e) => { verificarLetra(e) });
 });
 
-nivel[0].addEventListener('click', ()=>{elegirDificultad(0)} );
-nivel[1].addEventListener('click', ()=>{elegirDificultad(1)} );
-nivel[2].addEventListener('click', ()=>{elegirDificultad(2)} );
-$reglas.addEventListener('click', ()=> {$aiuda.classList.toggle('hidden')} );
+nivel[0].addEventListener('click', () => { elegirDificultad(0) });
+nivel[1].addEventListener('click', () => { elegirDificultad(1) });
+nivel[2].addEventListener('click', () => { elegirDificultad(2) });
+$reglas.addEventListener('click', () => { $aiuda.classList.toggle('hidden') });
 
 
 
